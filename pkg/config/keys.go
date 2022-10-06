8fec09a1425bbb6f60a119c9fe7936002cb1b30d
Julius Pedersen <deifyed@tutanota.com>
2022-10-06 21:54:25 +0200

    ✅ Add fs storage

--- pkg/config/keys.go
+++ pkg/config/keys.go
@@ -5,4 +5,6 @@ const (
 	LogLevel = "LogLevel"
 	// DotFilesDir defines the key for the directory where dotfiles (and directories) should be placed
 	DotFilesDir = "DotFilesDir"
+	// StorePath defines the key for the path to the infected paths store
+	StorePath = "StorePath"
 )
--- pkg/store/api.go
+++ pkg/store/api.go
@@ -1,5 +1,66 @@
 package store
 
-func Add(targetPath string, dotFilesPath string) error {
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/deifyed/infect/pkg/config"
+	"github.com/spf13/afero"
+	"github.com/spf13/viper"
+)
+
+type storage struct {
+	Paths map[string]string
+}
+
+func Add(fs *afero.Afero, targetPath string, dotFilesPath string) error {
+	store, err := open(fs)
+	if err != nil {
+		return fmt.Errorf("opening store: %w", err)
+	}
+
+	store.Paths[targetPath] = dotFilesPath
+
+	err = close(fs, store)
+	if err != nil {
+		return fmt.Errorf("closing store: %w", err)
+	}
+
+	return nil
+}
+
+func open(fs *afero.Afero) (storage, error) {
+	storePath := viper.GetString(config.StorePath)
+
+	content, err := fs.ReadFile(storePath)
+	if err != nil {
+		return storage{}, fmt.Errorf("reading store file: %w", err)
+	}
+
+	var store storage
+
+	err = json.Unmarshal(content, &store)
+	if err != nil {
+		return storage{}, fmt.Errorf("unmarshalling store file: %w", err)
+	}
+
+	return store, nil
+}
+
+// close saves the store to disk
+func close(fs *afero.Afero, store storage) error {
+	rawStore, err := json.Marshal(store)
+	if err != nil {
+		return fmt.Errorf("marshalling store: %w", err)
+	}
+
+	storePath := viper.GetString(config.StorePath)
+
+	err = fs.WriteReader(storePath, bytes.NewReader(rawStore))
+	if err != nil {
+		return fmt.Errorf("writing store to disk: %w", err)
+	}
+
 	return nil
 }
