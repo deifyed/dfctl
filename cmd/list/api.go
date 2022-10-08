70412a811c9fea96f1437d66d3e47136b7f9e1e2
Julius Pedersen <deifyed@tutanota.com>
2022-10-08 16:27:42 +0200

    ✅ Add sort and taint mark to list

--- cmd/list/api.go
+++ cmd/list/api.go
@@ -2,6 +2,7 @@ package list
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/deifyed/infect/pkg/storage"
 	"github.com/spf13/afero"
@@ -18,13 +19,45 @@ func RunE(fs *afero.Afero) func(cmd *cobra.Command, args []string) error {
 		if len(trackedPaths) == 0 {
 			fmt.Println("No tracked paths found")
 
-			return nil
+			trackedPaths = []storage.Path{
+				{
+					OriginalPath: "/home/deifyed/.config/nvim",
+					DotFilesPath: "/home/deifyed/.dotfiles/nvim",
+				},
+				{
+					OriginalPath: "/home/deifyed/.config/sway",
+					DotFilesPath: "/home/deifyed/.dotfiles/sway",
+					Taint:        true,
+				},
+				{
+					OriginalPath: "/home/deifyed/.config/alacritty",
+					DotFilesPath: "/home/deifyed/.dotfiles/alacritty",
+				},
+			}
+			//return nil
 		}
 
-		for index, trackedPath := range trackedPaths {
-			fmt.Printf("[%d] %s", index, trackedPath.OriginalPath)
+		sortedTrackedPaths := sortTrackedPaths(trackedPaths)
+
+		for index, trackedPath := range sortedTrackedPaths {
+			taint := ""
+
+			if trackedPath.Taint {
+				taint = " (tainted)"
+			}
+
+			fmt.Printf("[%d] %-50s %s\n", index, trackedPath.OriginalPath, taint)
 		}
 
 		return nil
 	}
+}
+
+// sortTrackedPaths sorts the tracked paths by their original path.
+func sortTrackedPaths(trackedPaths []storage.Path) []storage.Path {
+	sort.Slice(trackedPaths, func(i, j int) bool {
+		return trackedPaths[i].OriginalPath < trackedPaths[j].OriginalPath
+	})
+
+	return trackedPaths
 }
